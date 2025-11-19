a1febc2bdd8738daf5d9cbb6534884e7f3d6e125
pikkusami <sami00mikael@gmail.com>
2025-11-19 18:49:17 +0200

    fixes + imu logic corrected

--- src/template.c
+++ src/template.c
@@ -7,9 +7,14 @@
 #include <FreeRTOS.h>
 #include <queue.h>
 #include <task.h>
-#include <tusb.h>
 
+#include <tusb.h>
+#include "usbSerialDebug/helper.h"
 #include "tkjhat/sdk.h"
+
+#if CFG_TUSB_OS != OPT_OS_FREERTOS
+#error "This should be using FREERTOS but the CFG_TUSB_OS is not OPT_OS_FREERTOS"
+#endif
 
 // Default stack size for the tasks. It can be reduced to 1024 if task is not using lot of memory.
 #define DEFAULT_STACK_SIZE 2048
@@ -27,9 +32,11 @@ static void btn_fxn(uint gpio, uint32_t eventMask) {
     if (gpio == BUTTON1) {
         if (programState == IDLE) {
             programState = TRANSMITTING;
+            printf("Switched to TRANSMITTING");
         }
         else {
             programState = IDLE;
+            printf("Switched to IDLE");
         }
     }
     toggle_led(); // Kertoo onko nappia painettu ja onko tilassa TRANSMITTING/IDLE
@@ -65,32 +72,55 @@ void buzzerFnx(bool isDot) {
     }
 }
 
-
-//
-/* static void print_task(void *arg) {
+// Print example task
+static void print_task(void *arg) {
     (void)arg;
 
     float ax, ay, az, gx, gy, gz, t;
+    // Setting up the sensor. 
+    if (init_ICM42670() == 0) {
+        printf("ICM-42670P initialized successfully!\n");
+        if (ICM42670_start_with_default_values() != 0){
+            printf("ICM-42670P could not initialize accelerometer or gyroscope");
+        }
+    } else {
+        printf("Failed to initialize ICM-42670P.\n");
+    }
+
+    bool luettu = false;
     while(1) {
         if (programState == TRANSMITTING) {
             if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) == 0) {
-                if (ay < -200) {
+                sprintf(buffer,"Accel: X=%.2f, Y=%.2f, Z=%.2f | Gyro: X=%.2f, Y=%.2f, Z=%.2f| Temp: %2.2f°C\n", ax, ay, az, gx, gy, gz, t);
+                printf(buffer);
+                if (ax < -0.7 && luettu == false) {
                     // lähetetään piste
-                    usb_serial_print(".");
+                    printf(".");
                     ledFxn(true);
                     buzzerFnx(true);
-                }
-                else if (ay > 200) {
+                    luettu = true;
+                } else if (ax > 0.7 && luettu == false) {
                     // lähetetään viiva
-                    usb_serial_print("-");
+                    printf("-");
                     ledFxn(true);
                     buzzerFnx(true);
+                    luettu = true;
+                } else if (ay > 0.7 && luettu == false) {
+                    // lähetetään väli
+                    printf(" ");
+                    ledFxn(true);
+                    buzzerFnx(true);
+                    luettu = true;
+                } else if (ay > -0.7 && ay < 0.7) {
+                    luettu = false;
                 }
+            } else {
+                printf("Failed to read data");
             }
         }
-        vTaskDelay(pdMS_TO_TICKS(10));
+        vTaskDelay(pdMS_TO_TICKS(50));
     }
-} */
+}
 
 // ---- Task running USB stack ----
 static void usbTask(void *arg) {
@@ -110,12 +140,19 @@ int main() {
     init_hat_sdk();
     sleep_ms(300); //Wait some time so initialization of USB and hat is done.
 
-    init_red_led();
-    printf("Initializing red led\r\n");
     init_buzzer();
     printf("Initializing buzzer\r\n");
+    init_button1();
+    init_button2();
+    printf("Initializing buttons\r\n");
+    init_led();
+    init_red_led();
+    printf("Initializing leds\r\n");
 
-    /* TaskHandle_t printTask = NULL;
+    gpio_set_irq_enabled_with_callback(BUTTON1, GPIO_IRQ_EDGE_RISE, true, btn_fxn);
+    gpio_set_irq_enabled(BUTTON2, GPIO_IRQ_EDGE_RISE, true);
+
+    TaskHandle_t printTask, hUsb = NULL;
     // Create the tasks with xTaskCreate
     BaseType_t result = xTaskCreate(print_task,       // (en) Task function
                 "print_task",              // (en) Name of the task 
@@ -127,7 +164,20 @@ int main() {
     if(result != pdPASS) {
         printf("Example Task creation failed\n");
         return 0;
-    } */
+    }
+
+    xTaskCreate(usbTask, "usb", 1024, NULL, 3, &hUsb);
+    #if (configNUMBER_OF_CORES > 1)
+        vTaskCoreAffinitySet(hUsb, 1u << 0);
+    #endif
+
+    // VERY IMPORTANT, THIS SHOULD GO JUST BEFORE vTaskStartSheduler
+    // WITHOUT ANY DELAYS. OTHERWISE, THE TinyUSB stack wont recognize
+    // the device.
+    // Initialize TinyUSB 
+    tusb_init();
+
+    usb_serial_init();
 
     // Start the scheduler (never returns)
     vTaskStartScheduler();
